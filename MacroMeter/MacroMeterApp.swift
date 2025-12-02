f06eb3ef01db0128fe35394ac9a1c7e1f1df03d2
Andy Huoy <anhuo@unc.edu>
2025-12-02 09:53:08 -0500

    Initial Commit

---
+++ MacroMeter/MacroMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroMeterApp.swift
+//  MacroMeter
+//
+//  Created by Andy Huoy on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MacroMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
